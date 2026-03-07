1210c13f089ecedebc040e8e60e72136062c9737
Afliday <aflidaywork@gmail.com>
2026-03-07 13:11:18 +0300

    Adding_Router_Video

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import { db } from "./db";
 import { resolve } from 'path';
+import { videoRouter } from './video.router'
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,110 +9,11 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Samurai')
 })
-app.get("/videos", (req: Request, res: Response) => {
-        res.status(200).send(db.videos);
-    });
-app.get("/videos/:id", (req: Request, res: Response) => {
-  const video = db.videos.find(f_video => f_video.id === +req.params.id);
 
-  if (!video) {
-    res.status(404).send({ message: "Video Not Found" });
-    return;
-  }
+app.use("/videos", videoRouter)
 
-  res.status(200).send(video);
-});
-app.post("/videos", (req: Request, res: Response) => {
-  const { title, author, availableResolutions, canBeDownloaded } = req.body;
-  const errors: { field: string, message: string }[] = [];
-  const validResolutions = ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"];
-  if (!title || title.trim() === "" || title.length > 40) {
-    errors.push({ field: "title", message: "Invalid title" });
-  }
 
-  if (canBeDownloaded !== undefined && typeof canBeDownloaded !== "boolean") {
-    errors.push({ field: "canBeDownloaded", message: "Invalid canBeDownloaded" });
-  }
-  if (!author || author.trim() === "" || author.length > 20) {
-    errors.push({ field: "author", message: "Invalid author" });
-  }
-
-  if (!availableResolutions || !Array.isArray(availableResolutions) || availableResolutions.length === 0 || !availableResolutions.every((resolut: string) => validResolutions.includes(resolut))) {
-    errors.push({ field: "availableResolutions", message: "Invalid resolutions" });
-  }
-
-  if (errors.length > 0) {
-    res.status(400).send({ errorsMessages: errors });
-    return;
-  }
-
-  const newVideo = {
-    id: db.videos.length ? db.videos[db.videos.length - 1].id + 1 : 1,
-    title,
-    author,
-    canBeDownloaded: canBeDownloaded ?? false,
-    minAgeRestriction: null,
-    createdAt: new Date().toISOString(),
-    publicationDate: new Date(Date.now() + 86400000).toISOString(),
-    availableResolutions
-  };
-
-  db.videos.push(newVideo);
-  res.status(201).send(newVideo);
-});
-app.put("/videos/:id", (req: Request, res: Response) => {
-  const video = db.videos.find(v => v.id === +req.params.id);
-  const { title, author, availableResolutions, canBeDownloaded, minAgeRestriction, publicationDate } = req.body;
-  const errors: { field: string, message: string }[] = [];
-
-  if (!video) {
-    res.sendStatus(404);
-    return;
-  }
-  if (canBeDownloaded !== undefined && typeof canBeDownloaded !== "boolean") {
-    errors.push({ field: "canBeDownloaded", message: "Invalid canBeDownloaded" });
-  }
-  if (!title || title.trim() === "" || title.length > 40) {
-    errors.push({ field: "title", message: "Invalid title" });
-  }
-  if (!author || author.trim() === "" || author.length > 20) {
-    errors.push({ field: "author", message: "Invalid author" });
-  }
-  if (!availableResolutions || !Array.isArray(availableResolutions) || availableResolutions.length === 0) {
-    errors.push({ field: "availableResolutions", message: "Invalid resolutions" });
-  }
-  if (minAgeRestriction !== null && (typeof minAgeRestriction !== "number" || minAgeRestriction < 1 || minAgeRestriction > 18)) {
-    errors.push({ field: "minAgeRestriction", message: "Invalid minAgeRestriction" })
-  }
-  if (publicationDate !== undefined && typeof publicationDate !== "string") {
-    errors.push({ field: "publicationDate", message: "Invalid publicationDate" });
-  }
-  if (errors.length > 0) {
-    res.status(400).send({ errorsMessages: errors });
-    return;
-  }
-
-  video.title = title ?? video.title;
-  video.author = author ?? video.author;
-  video.availableResolutions = availableResolutions ?? video.availableResolutions;
-  video.canBeDownloaded = canBeDownloaded ?? video.canBeDownloaded;
-  video.minAgeRestriction = minAgeRestriction ?? video.minAgeRestriction;
-  video.publicationDate = publicationDate ?? video.publicationDate;
-
-  res.sendStatus(204);
-});
-// ИСПРАВЛЕНО: "vedios" -> "videos"
-app.delete("/videos/:id", (req: Request, res: Response) => {
-  const check = db.videos.find(v => v.id === +req.params.id);
-
-  if (!check) {
-    res.sendStatus(404);
-    return;
-  }
-
-  db.videos = db.videos.filter(v => v.id !== +req.params.id);
-  res.sendStatus(204);
-});
+// 
 
 app.delete("/testing/all-data", (req: Request, res: Response) => {
   db.videos = [];
